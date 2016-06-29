d4a36d6a121116379e2eca147f390a788d0bab78
Hank Wang <drapho@gmail.com>
2016-06-29 20:56:33 +0800

    [python] more currency and autopep8

--- python/captureexchangerate/CaptureBotRate.py
+++ python/captureexchangerate/CaptureBotRate.py
@@ -11,33 +11,36 @@ from datetime import datetime
 import time
 import re
 
-currencyList = ['ZAR', 'AUD', 'NZD', 'CAD', 'SEK', 'USD']
+currencyList = ["JPY", "GBP", "ZAR", "AUD", "NZD", "CAD", "SEK", "USD"]
+
 
 def getCurrencyRate():
-	if isNormalDay():
-		url = "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm"
-		data = urllib2.urlopen(url).read()
-		print data
-		soup = BeautifulSoup(data)
+    if isNormalDay():
+        url = "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm"
+        data = urllib2.urlopen(url).read()
+        print data
+        soup = BeautifulSoup(data)
 
-		dict = {
-			'date': time.strftime("%Y-%m-%d", time.localtime())
-		}
-		for i in soup.findAll('td', { 'class' : 'titleLeft'}):
-			title = i.find('img').next.encode('utf-8')
-			currency = re.search('\(([\w]+)\)', title).group(1)
-			if currency in currencyList:
-				dict[currency.lower()] = i.parent.findAll('td')[3].renderContents()
+        dict = {
+            'date': time.strftime("%Y-%m-%d", time.localtime())
+        }
+        for i in soup.findAll('td', {'class': 'titleLeft'}):
+            title = i.find('img').next.encode('utf-8')
+            currency = re.search('\(([\w]+)\)', title).group(1)
+            if currency in currencyList:
+                dict[currency.lower()] = i.parent.findAll('td')[
+                    3].renderContents()
 
-		return dict
+        return dict
 
-	else:
-		return None
+    else:
+        return None
 
 
 def isNormalDay():
-	return datetime.now().weekday() < 5
+    return datetime.now().weekday() < 5
 
 
 if __name__ == "__main__":
-	print getCurrencyRate()
+    # 即期買入
+    print getCurrencyRate()
