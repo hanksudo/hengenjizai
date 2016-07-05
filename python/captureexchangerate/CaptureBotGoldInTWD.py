3ab4dd7f2c473d35ca01b80f6236dd88740de342
Hank Wang <drapho@gmail.com>
2016-07-05 18:31:26 +0800

    [python] improve Capture Bot Gold and Currency

--- python/captureexchangerate/CaptureBotGoldInTWD.py
+++ python/captureexchangerate/CaptureBotGoldInTWD.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env/ python
 # -*- coding: utf-8 -*-
+#
+# 取得台銀台幣黃金銀行買入賣出價
 
-import json
+
 import re
 import time
 import urllib2
 
 from BeautifulSoup import BeautifulSoup
-from pygments import highlight
-from pygments.formatters import Terminal256Formatter
-from pygments.lexers.sql import PostgresLexer
-from pygments.lexers.web import JsonLexer
+from utils import highlight_json
 
-
-'''
-取得台銀台幣黃金銀行買入賣出價
-buy mean bank buy
-sell mean bank sell
-'''
 
 def getGoldPrice():
     url = "http://rate.bot.com.tw/Pages/Static/UIP005.zh-TW.htm"
@@ -25,21 +18,15 @@ def getGoldPrice():
     soup = BeautifulSoup(data)
 
     response = {
-        'date': time.strftime("%Y-%m-%d", time.localtime())
+        "date": time.strftime("%Y-%m-%d", time.localtime())
     }
 
-    prices = soup.findAll('td', { 'class' : 'decimal' }, text = re.compile("^\d+$"))
-    response['sell'] = prices[0]
-    response['buy'] = prices[1]
+    prices = soup.findAll("td", {"class": "decimal"}, text=re.compile("^\d+$"))
+    response["sell"] = prices[0]
+    response["buy"] = prices[1]
 
     return response
 
-def highlight_json(json_str_or_obj):
-    """http://pygments.org/docs/"""
-    if isinstance(json_str_or_obj, dict):
-        json_str_or_obj = json.dumps(json_str_or_obj, indent=4)
-        json_str_or_obj = json_str_or_obj.decode("unicode_escape").encode("utf8")
-        return highlight(json_str_or_obj, JsonLexer(), Terminal256Formatter())
 
 if __name__ == "__main__":
     print highlight_json(getGoldPrice())
--- python/captureexchangerate/CaptureBotRate.py
+++ python/captureexchangerate/CaptureBotRate.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-'''
-取得台銀貨幣即期銀行買入價
-'''
+#
+# 取得台銀貨幣價格
 
 import urllib2
 from BeautifulSoup import BeautifulSoup
+from utils import highlight_json
 from datetime import datetime
 import time
 import re
@@ -18,21 +17,25 @@ def getCurrencyRate():
     if isNormalDay():
         url = "http://rate.bot.com.tw/Pages/Static/UIP003.zh-TW.htm"
         data = urllib2.urlopen(url).read()
-        print data
         soup = BeautifulSoup(data)
 
-        dict = {
-            'date': time.strftime("%Y-%m-%d", time.localtime())
+        response = {
+            "date": time.strftime("%Y-%m-%d", time.localtime()),
+            "buy_cash": {},
+            "sell_cash": {},
+            "buy_spot": {},
+            "sell_spot": {}
         }
-        for i in soup.findAll('td', {'class': 'titleLeft'}):
-            title = i.find('img').next.encode('utf-8')
-            currency = re.search('\(([\w]+)\)', title).group(1)
+        for i in soup.findAll("td", {"class": "titleLeft"}):
+            title = i.find("img").next.encode("utf-8")
+            currency = re.search("\(([\w]+)\)", title).group(1)
             if currency in currencyList:
-                dict[currency.lower()] = i.parent.findAll('td')[
-                    3].renderContents()
+                response["buy_cash"][currency.lower()] = i.parent.findAll("td")[1].renderContents()
+                response["sell_cash"][currency.lower()] = i.parent.findAll("td")[2].renderContents()
+                response["buy_spot"][currency.lower()] = i.parent.findAll("td")[3].renderContents()
+                response["sell_spot"][currency.lower()] = i.parent.findAll("td")[4].renderContents()
 
-        return dict
-
+        return response
     else:
         return None
 
@@ -43,4 +46,4 @@ def isNormalDay():
 
 if __name__ == "__main__":
     # 即期買入
-    print getCurrencyRate()
+    print highlight_json(getCurrencyRate())
