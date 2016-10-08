356bd2223bfe5dc45d340b56e12e5cc82a720762
Hank Wang <drapho@gmail.com>
2016-10-08 20:41:50 +0800

    [Swift] initial Calculator

--- iOS-Swift/Calculator/Calculator/ViewController.swift
+++ iOS-Swift/Calculator/Calculator/ViewController.swift
@@ -2,62 +2,24 @@
 //  ViewController.swift
 //  Calculator
 //
-//  Created by Hank Wang on 10/29/15.
-//  Copyright © 2015 hanksudo. All rights reserved.
+//  Created by Hank Wang on 10/8/16.
+//  Copyright © 2016 hanksudo. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var display: UILabel!
-    
-    var userIsInTheMiddleOfTypingANumber = false
-    
-    var brain = CalculatorBrain()
-    
-    @IBAction func appendDigit(sender: UIButton) {
-        let digit = sender.currentTitle!
-        if userIsInTheMiddleOfTypingANumber {
-            
-            display.text = display.text! + digit
-        } else {
-            display.text = digit
-            userIsInTheMiddleOfTypingANumber = true
-        }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    
-    @IBAction func operate(sender: UIButton) {
-        if userIsInTheMiddleOfTypingANumber {
-            enter()
-        }
-        if let operation = sender.currentTitle {
-            if let result = brain.performOperation(operation) {
-                displayValue = result
-            } else {
-                displayValue = 0
-            }
-        }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func enter() {
-        userIsInTheMiddleOfTypingANumber = false
-        if let result = brain.pushOperand(displayValue) {
-            displayValue = result
-        } else {
-            displayValue = 0
-        }
-    }
-    
-    var displayValue: Double {
-        get {
-            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
-        }
-        set {
-            display.text = "\(newValue)"
-            userIsInTheMiddleOfTypingANumber = false
-        }
-    }
+
+
 }
 
