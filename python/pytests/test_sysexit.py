d50a71a459221bda832e39710126556d5c5df240
hanksudo <drapho@gmail.com>
2017-10-08 21:45:36 +0900

    [python] add pytests

---
+++ python/pytests/test_sysexit.py
@@ -0,0 +1,9 @@
+# Asserting that a certain exception is raised
+
+import pytest
+def f():
+    raise SystemExit(1)
+
+def test_mytest():
+    with pytest.raises(SystemExit):
+        f()
---
+++ python/pytests/test_tmpdir.py
@@ -0,0 +1,3 @@
+def test_needfiles(tmpdir):
+    print(tmpdir)
+    assert 0
