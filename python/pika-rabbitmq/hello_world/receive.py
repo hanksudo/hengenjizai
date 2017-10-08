330843457a7231cdb69f3e3840f38e92ab533971
hanksudo <drapho@gmail.com>
2017-10-08 23:48:58 +0900

    [python] update pika rabbitmq demo

--- python/pika-rabbitmq/hello_world/receive.py
+++ python/pika-rabbitmq/hello_world/receive.py
@@ -3,13 +3,15 @@ import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
+
 channel.queue_declare(queue='hello')
 
-print ' [*] Waiting for messages. To exit press CTRL+C'
-
-
 def callback(ch, method, properties, body):
-    print " [x] Received %r" % (body,)
+    print(" [x] Received %r" % body)
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume(callback,
+                      queue='hello',
+                      no_ack=True)
+                      
+print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- python/pika-rabbitmq/hello_world/send.py
+++ python/pika-rabbitmq/hello_world/send.py
@@ -3,9 +3,13 @@ import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
+
 channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 
-print " [x] Sent 'Hello World!'"
-
+for i in range(5):
+    message = 'Hello World! %d' % (i + 1)
+    channel.basic_publish(exchange='',
+                          routing_key='hello',
+                          body=message)
+    print(" [x] Sent {}".format(message))
 connection.close()
