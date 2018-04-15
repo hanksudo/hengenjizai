d3e94b153da716aaa31e07672d16d9b365907f2c
Hank Wang <drapho@gmail.com>
2018-04-16 05:15:17 +0900

    feat: update machine learning recipes

--- python/machine-learning-recipes/02 - visualizing_decision_tree.py
+++ python/machine-learning-recipes/02 - visualizing_decision_tree.py
@@ -1,31 +1,40 @@
+# https://en.wikipedia.org/wiki/Iris_flower_data_set
+
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn import tree
 from sklearn.externals.six import StringIO
 import pydot
 
+# 1. Import dataset 
 iris = load_iris()
-#print(iris.feature_names)
-#print(iris.target_names)
-#print(iris.data[0])
-#print(iris.target[0])
+# print(iris.feature_names)
+# print(iris.target_names)
+# print(iris.data[0])
+# print(iris.target[0])
+# for i in range(len(iris.target)):
+#     print("Example {}: label {}, features {}".format(i, iris.target[i], iris.data[i]))
+
 test_idx = [0, 50, 100]
 
-# training data
+# 2. Train a classifier
 train_target = np.delete(iris.target, test_idx)
 train_data = np.delete(iris.data, test_idx, axis=0)
 
 # testing data
+# - Example userd to "test" the classifier's accuracy.
+# - Not part of the training data.
 test_target = iris.target[test_idx]
 test_data = iris.data[test_idx]
 
+# 3. Predict label for new flower
 clf = tree.DecisionTreeClassifier()
 clf.fit(train_data, train_target)
 
 print(test_target)
 print(clf.predict(test_data))
 
-# viz code
+# 4. Visualize the tree
 dot_data = StringIO()
 tree.export_graphviz(
     clf,
@@ -37,3 +46,6 @@ tree.export_graphviz(
 )
 graph = pydot.graph_from_dot_data(dot_data.getvalue())
 graph[0].write_pdf("iris.pdf")
+
+# print(test_data[0], test_target[0])
+# print(iris.feature_names, iris.target_names)
---
+++ python/machine-learning-recipes/README.md
@@ -0,0 +1,11 @@
+# Machine Learning Recipes
+
+- [Machine Learning Recipes with Josh Gordon - YouTube](https://www.youtube.com/playlist?list=PLOU2XLYxmsIIuiBfYad6rFYQU_jL2ryal)
+
+## Notes
+
+Collect Training data -> Train Classifier -> Make Predictions
+
+## Links
+
+- [scikit-learn: machine learning in Python — scikit-learn 0.16.1 documentation](http://scikit-learn.org/)
