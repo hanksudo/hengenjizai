20712a38cd1bc424e74e220671352ceed11038b6
Hank Wang <drapho@gmail.com>
2018-04-18 23:46:58 +0900

    update content

--- python/machine-learning-recipes/04-pipeline.py
+++ python/machine-learning-recipes/04-pipeline.py
@@ -10,10 +10,16 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5)
 
-# my_classifier = tree.DecisionTreeClassifier()
+my_classifier = tree.DecisionTreeClassifier()
+my_classifier.fit(X_train, y_train)
+
+predictions = my_classifier.predict(X_test)
+print(accuracy_score(y_test, predictions))
+
+
+# by KNN
 my_classifier = KNeighborsClassifier()
 my_classifier.fit(X_train, y_train)
 
 predictions = my_classifier.predict(X_test)
-
 print(accuracy_score(y_test, predictions))
