eacfb433fd2dc9b4be653a6a7b8c55c9073720cd
Hank Wang <drapho@gmail.com>
2018-04-26 12:49:29 +0900

    add MonkeyPinch

---
+++ ios/demos/MonkeyPinch/MonkeyPinch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MonkeyPinch
+//
+//  Created by Hank Wang on 12/9/10.
+//  Copyright (c) 2012年 hankwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)handlePan:(UIPanGestureRecognizer *)recognizer;
+
+@end
---
+++ ios/demos/MonkeyPinch/MonkeyPinch/ViewController.m
@@ -0,0 +1,59 @@
+//
+//  ViewController.m
+//  MonkeyPinch
+//
+//  Created by Hank Wang on 12/9/10.
+//  Copyright (c) 2012年 hankwang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)handlePan:(UIPanGestureRecognizer *)recognizer
+{
+    CGPoint translation = [recognizer translationInView:self.view];
+    recognizer.view.center = CGPointMake(recognizer.view.center.x + translation.x,
+                                         recognizer.view.center.y + translation.y);
+    [recognizer setTranslation:CGPointMake(0, 0) inView:self.view];
+    
+    if (recognizer.state == UIGestureRecognizerStateEnded)
+    {
+        CGPoint velocity = [recognizer velocityInView:self.view];
+        CGFloat magnitude = sqrtf((velocity.x * velocity.x) + (velocity.y * velocity.y));
+        CGFloat slideMult = magnitude / 200;
+        NSLog(@"magnitude: %f, slideMult: %f", magnitude, slideMult);
+        
+        float slideFactor = 0.1 * slideMult; // increase for more of a slide
+        CGPoint finalPoint = CGPointMake(recognizer.view.center.x + (velocity.x + slideFactor),
+                                         recognizer.view.center.y + (velocity.y + slideFactor));
+        
+        [UIView animateWithDuration:slideFactor*2 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+            recognizer.view.center = finalPoint;
+        } completion:nil];
+    }
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+
+@end
