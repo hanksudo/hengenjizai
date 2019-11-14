9b34042d3527180af8c65837bf13280c548ee06e
Hank Wang <drapho@gmail.com>
2019-11-14 19:57:31 +0800

    feat: add ddd car manufacturer sample

---
+++ python/ddd_car_manufacturer.py
@@ -0,0 +1,56 @@
+from abc import ABCMeta
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+# DDD
+@dataclass(frozen=True)
+class ValueObject:
+    pass
+
+class Entity:
+    pass
+
+
+# Car models
+
+class CarColor(Enum):
+    RED = "red"
+
+class CarType(Enum):
+    PRIUS = "prius"
+
+@dataclass(frozen=True)
+class Car(ValueObject):
+    brand: str
+    color: str
+    name: str
+
+    colors = CarColor
+    types = CarType
+
+class CarFactory(metaclass=ABCMeta):
+    def create(self):
+        pass
+
+class CarManufacturer(Entity):
+    cars: List[Car] = []
+
+    def __init__(self, car_factory: CarFactory):
+        self.car_factory = car_factory
+
+    def construct_cars(self, num_of_cars: int):
+        for _ in range(num_of_cars):
+            self.cars.append(self.car_factory.create())
+
+class PriusCarFactory(CarFactory):
+    brand = Car.types.PRIUS
+    color = Car.colors.RED
+    name = "My favorite car"
+    
+    def create(self):
+        return Car(self.brand, self.color, self.name)
+
+if __name__ == "__main__":
+    manufacturer = CarManufacturer(PriusCarFactory())
+    manufacturer.construct_cars(num_of_cars=40)
