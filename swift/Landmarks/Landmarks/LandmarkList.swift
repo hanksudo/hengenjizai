bfc10ff79097f77cff97703e9ce47d8304a29fec
Hank Wang <drapho@gmail.com>
2020-03-03 21:11:48 +0900

    [SwiftUI] Handling User Input - add toggle control

--- swift/Landmarks/Landmarks/LandmarkList.swift
+++ swift/Landmarks/Landmarks/LandmarkList.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State var showFavoritesOnly = false
+    
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                        LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(landmarkData) { landmark in
+                    if !self.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                                LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
             .navigationBarTitle(Text("Landmarks"))
