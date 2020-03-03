7489ed35424c775fa5bf591ad011fdd6f4c73a26
Hank Wang <drapho@gmail.com>
2020-03-03 23:46:54 +0900

    [SwiftUI] Drawing Paths and Shapes

---
+++ swift/Landmarks/Landmarks/Supporting Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Hank Wang on 2020/3/3.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
