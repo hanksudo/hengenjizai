4ac53ae67aaf2c9285929a577d8833d3142afb9b
Hank Wang <drapho@gmail.com>
2020-03-19 22:57:02 +0900

    [ruby] array max/min element

--- ruby/ruby-101/array.rb
+++ ruby/ruby-101/array.rb
@@ -2,8 +2,11 @@ my_array = [1, 2, 3]
 my_array.push(4)
 my_array << 5 # same as above
 print my_array
-print (1..3).to_a
+print(1..3).to_a
 puts
+
+puts my_array.max
+puts my_array.min
 
 multi_d_array = [
   [0, 0, 0, 0],
