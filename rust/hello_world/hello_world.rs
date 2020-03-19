1b8e7dfaee600d768b1da2ed42c84320362dba20
Hank Wang <drapho@gmail.com>
2020-03-19 16:14:51 +0900

    [rust] update print and format

--- rust/hello_world/hello_world.rs
+++ rust/hello_world/hello_world.rs
@@ -1,8 +1,13 @@
+/*
+    block comment
+*/
+
+// This is the main function
 fn main() {
     // println is a Rust macro
     println!("Hello, world!");
     println!("{}, {}!", "Hello", "world"); // Hello, world!
-    println!("{0}, {1}!", "Hello", "world"); // Hello, world!
+    println!("{0}, {1}! {1} {0}", "Hello", "world"); // Hello, world!
     println!("{greeting}, {name}!", greeting = "Hello", name = "world"); // Hello, world!
 
     println!("{:?}", [1, 2, 3]); // [1, 2, 3]
@@ -18,4 +23,21 @@ fn main() {
     // The format! marco is used to store the formatted string.
     let x = format!("{}, {}!", "Hello", "world");
     println!("{}", x); // Hello, world!
+
+    // Special formatting
+    println!("{} {:b} binary", 1, 15);
+
+    // right align
+    println!("{number:>width$}", number=1, width=6);
+
+    // pad number
+    println!("{number:>0width$}", number=1, width=6);
+
+    const PI: f64 = 3.1415926;
+    println!("Pi is roughly {:.3}", PI);
+
+    // Print custom type
+    #[derive(Debug)]
+    struct Structure(i32);
+    println!("print struct {:?}", Structure(3));
 }
