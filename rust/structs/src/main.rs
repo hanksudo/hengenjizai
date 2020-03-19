6164fbeacb4daddf7300588d73e3440a9ca0f391
Hank Wang <drapho@gmail.com>
2020-03-19 22:56:50 +0900

    [rust] structs and tuple struct

---
+++ rust/structs/src/main.rs
@@ -0,0 +1,60 @@
+struct User {
+    username: String,
+    email: String,
+    active: bool,
+    sign_in_count: u64
+}
+
+fn add_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1
+    }
+}
+
+fn add_user_shorthand(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1
+    }
+}
+
+fn main() {
+    let user1 = User {
+        email: String::from("hank@myemail.com"),
+        username: String::from("hanksudo"),
+        active: true,
+        sign_in_count: 1
+    };
+
+    let user2 = add_user(
+        String::from("hank2@myemail.com"), 
+        String::from("hank2"));
+    
+    let user3 = add_user_shorthand(
+        String::from("hank3@myemail.com"), 
+        String::from("hank3"));
+
+    let user_by_struct_update = User {
+        email: String::from("new-email@gmail.com"),
+        username: String::from("new-useranme"),
+        ..user1
+    };
+
+    print_user_info(user1);
+    print_user_info(user2);
+    print_user_info(user3);
+    print_user_info(user_by_struct_update);
+}
+
+fn print_user_info(user: User) {
+    println!("{username} <{email}> active: {active}, {sign_in_count}", 
+        username=user.username, 
+        email=user.email, 
+        active=user.active,
+        sign_in_count=user.sign_in_count);    
+}
---
+++ rust/structs_rectangle/src/main.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!("rect1 is {:?}", rect1);
+    println!("The area of rectangle is {} square pixels.", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
