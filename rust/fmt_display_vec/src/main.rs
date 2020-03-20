77d2d949286e644d5fb79919320ff10a8db98f8a
Hank Wang <drapho@gmail.com>
2020-03-20 18:25:07 +0900

    [rust] add fmt::Display for vec!

---
+++ rust/fmt_display_vec/src/main.rs
@@ -0,0 +1,25 @@
+// https://doc.rust-lang.org/rust-by-example/hello/print/print_display/testcase_list.html
+use std::fmt;
+
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}: {}", count, v)?;
+        }
+
+        // Close open bracket and return a fmt::Result value.
+        write!(f, "]")
+    }
+}
+
+fn main() {
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+}
