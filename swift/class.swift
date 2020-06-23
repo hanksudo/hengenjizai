5c12fe17c91c2854810eddccadf217a3861b4b09
Hank Wang <drapho@gmail.com>
2020-06-24 01:06:20 +0900

    [swift] class

---
+++ swift/class.swift
@@ -0,0 +1,10 @@
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+var shape = Shape()
+shape.numberOfSides = 7
+print(shape.simpleDescription())
