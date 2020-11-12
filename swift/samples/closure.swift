7417376090d4c99cab0b80223b83e84890aec23d
Hank Wang <drapho@gmail.com>
2020-11-12 10:43:36 +0900

    [swift] update closure

--- swift/samples/closure.swift
+++ swift/samples/closure.swift
@@ -13,3 +13,39 @@ print(mappedNumbers)
 // refer by number
 let sortedNumbers = numbers.sorted { $0 > $1 }
 print(sortedNumbers)
+
+
+/**
+ Sort method
+**/
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+// normal function
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+
+var reversedNames = names.sorted(by: backward)
+print(reversedNames)
+
+// Closure expression
+reversedNames = names.sorted(by: { (s1: String, s2: String) -> Bool in
+    return s1 > s2
+})
+print(reversedNames)
+
+// Infer type from context
+reversedNames = names.sorted(by: { s1, s2 in return s1 > s2 })
+print(reversedNames)
+
+// Implicit returns
+reversedNames = names.sorted(by: { s1, s2 in s1 > s2 })
+print(reversedNames)
+
+// Shorthand argument names
+reversedNames = names.sorted(by: { $0 > $1 })
+print(reversedNames)
+
+// Operator methods
+reversedNames = names.sorted(by: >)
+print(reversedNames)
