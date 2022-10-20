f4e9fbb037040483e27471683da84f1627e072f5
Hank Wang <467745+hanksudo@users.noreply.github.com>
2022-10-21 00:28:51 +0900

    reactjs: add random-color-picker

---
+++ js/reactjs/random-color-picker/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function App(props) {
+  const [color, setColor] = React.useState([123, 123, 123]);
+
+  React.useEffect(() => {
+    document.body.style.background = formatColor(color);
+  })
+
+  const formatColor = (arr) => "rgb(" + arr.join(", ") + ")";
+  const changeColor = () => {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random() * 256));
+    }
+    setColor(random);
+  }
+
+  return (
+    <div>
+      <h1>Current color: {formatColor(color)}</h1>
+      <Button onClick={changeColor} />
+    </div>
+  )
+}
+
+function Button(props) {
+  return (
+    <button onClick={props.onClick}>Chnage color</button>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
