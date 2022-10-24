2a208a8be34c415c63731fe64742ddb32a664aee
Hank Wang <467745+hanksudo@users.noreply.github.com>
2022-10-24 22:54:36 +0900

    add passing thought

---
+++ js/reactjs/passing-thought/src/Thought.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+
+export function Thought(props) {
+  const { thought, removeThought } = props;
+
+  const handleRemoveClick = () => {
+    removeThought(thought.id);
+  };
+
+  useEffect(() => {
+    const timeRemaining = thought.expiresAt - Date.now();
+    const timeoutId = setTimeout(() => props.removeThought(thought.id), timeRemaining);
+  
+    return () => {
+      clearTimeout(timeoutId);
+    }
+  }, [])
+
+  return (
+    <li className="Thought">
+      <button
+        aria-label="Remove thought"
+        className="remove-button"
+        onClick={handleRemoveClick}
+      >
+        &times;
+      </button>
+      <div className="text">{thought.text}</div>
+    </li>
+  );
+}
