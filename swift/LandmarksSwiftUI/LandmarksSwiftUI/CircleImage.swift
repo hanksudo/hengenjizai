d325ae94aea5dda8218ecef78ee450a6c430e29d
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-04-27 01:58:26 +0900

    finish section 4: custom SwiftUI view

---
+++ swift/LandmarksSwiftUI/LandmarksSwiftUI/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  LandmarksSwiftUI
+//
+//  Created by Hank Wang on 2023/04/27.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- swift/LandmarksSwiftUI/LandmarksSwiftUI/ContentView.swift
+++ swift/LandmarksSwiftUI/LandmarksSwiftUI/ContentView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
         .padding()
     }
