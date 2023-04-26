fc11333fe08a76ef511589fafb7a3a502b84328e
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-04-27 01:30:09 +0900

    init SwiftUI version of landmarks

---
+++ swift/Landmarks-swiftui/Landmarks-swiftui/Landmarks_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_swiftuiApp.swift
+//  Landmarks-swiftui
+//
+//  Created by Hank Wang on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
