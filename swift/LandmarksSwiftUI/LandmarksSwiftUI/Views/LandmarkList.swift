d9d0ac87a71099de4494567c6e6b85937914019a
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-04-28 23:34:32 +0900

    add toggle favorite

--- swift/LandmarksSwiftUI/LandmarksSwiftUI/Views/LandmarkList.swift
+++ swift/LandmarksSwiftUI/LandmarksSwiftUI/Views/LandmarkList.swift
@@ -18,14 +18,20 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorites only")
                 }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .navigationTitle("Landmarks")
             }
-            .navigationTitle("Landmarks")
         }
     }
 }
