155928b3873bbe8178ce998d19019a65c73ee094
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-04-28 22:40:49 +0900

    filter by favorite

--- swift/LandmarksSwiftUI/LandmarksSwiftUI/Views/LandmarkList.swift
+++ swift/LandmarksSwiftUI/LandmarksSwiftUI/Views/LandmarkList.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoriteOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoriteOnly) || landmark.isFavorite
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
@@ -24,10 +32,6 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone 14", "iPhone 14 Pro Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+        LandmarkList()
     }
 }
--- swift/LandmarksSwiftUI/LandmarksSwiftUI/Views/LandmarkRow.swift
+++ swift/LandmarksSwiftUI/LandmarksSwiftUI/Views/LandmarkRow.swift
@@ -16,6 +16,13 @@ struct LandmarkRow: View {
                 .resizable()
                 .frame(width: 50, height: 50)
             Text(landmark.name)
+            
+            Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
