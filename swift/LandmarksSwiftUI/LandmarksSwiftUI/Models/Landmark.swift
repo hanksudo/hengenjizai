7508c302d20cd8de1318417f01180363d884061d
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-04-28 17:34:25 +0900

    feat: add model and restructure folder

---
+++ swift/LandmarksSwiftUI/LandmarksSwiftUI/Models/Landmark.swift
@@ -0,0 +1,35 @@
+//
+//  Landmark.swift
+//  LandmarksSwiftUI
+//
+//  Created by Hank Wang on 2023/04/28.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
