5615c72ae59765ed95142c70407271ba6d1e0f7e
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-04-28 22:31:29 +0900

    dynamic preview

--- swift/LandmarksSwiftUI/LandmarksSwiftUI/Views/LandmarkList.swift
+++ swift/LandmarksSwiftUI/LandmarksSwiftUI/Views/LandmarkList.swift
@@ -24,6 +24,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 14", "iPhone 14 Pro Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
