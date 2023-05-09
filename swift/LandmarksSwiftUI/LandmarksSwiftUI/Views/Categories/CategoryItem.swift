4f424458d7f4eb6c6feeeb98dc708bbd794d39c0
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-05-09 15:07:42 +0900

    swiftui: categories

---
+++ swift/LandmarksSwiftUI/LandmarksSwiftUI/Views/Categories/CategoryItem.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryItem.swift
+//  LandmarksSwiftUI
+//
+//  Created by Hank Wang on 2023/05/09.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .foregroundColor(.primary)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem(landmark: ModelData().landmarks[0])
+    }
+}
