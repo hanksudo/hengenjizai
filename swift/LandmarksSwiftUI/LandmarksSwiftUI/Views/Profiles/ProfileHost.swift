fae24496e9bbdc0bc705410cd6ba71264aaf2322
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-05-09 16:02:53 +0900

    swiftui: profile editor

--- swift/LandmarksSwiftUI/LandmarksSwiftUI/Views/Profiles/ProfileHost.swift
+++ swift/LandmarksSwiftUI/LandmarksSwiftUI/Views/Profiles/ProfileHost.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: draftProfile)
+            HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
+            }
         }
         .padding()
     }
