e6d718a8de6fd1ddbc8b6e7a3fb92bb84826a2d9
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-06-11 17:00:32 +0900

    landmarks: add custom commands for favorite

--- swift/LandmarksSwiftUI/LandmarksSwiftUI/LandmarksSwiftUIApp.swift
+++ swift/LandmarksSwiftUI/LandmarksSwiftUI/LandmarksSwiftUIApp.swift
@@ -16,6 +16,11 @@ struct LandmarksSwiftUIApp: App {
             ContentView()
                 .environmentObject(modelData)
         }
+        #if !os(watchOS)
+        .commands {
+            LandmarkCommands()
+        }
+        #endif
         
         #if os(watchOS)
         WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
---
+++ swift/LandmarksSwiftUI/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,36 @@
+//
+//  LandmarkCommands.swift
+//  MacLandmarks
+//
+//  Created by Hank Wang on 2023/06/11.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    @FocusedBinding(\.selectedLandmark) var selectedLandmark
+    
+    var body: some Commands {
+        SidebarCommands()
+        
+        CommandMenu("Landmark") {
+            Button("\(selectedLandmark?.isFavorite == true ? "Remove" : "Mark") as Favorite") {
+                selectedLandmark?.isFavorite.toggle()
+            }
+            .keyboardShortcut("f", modifiers: [.shift, .option])
+            .disabled(selectedLandmark == nil)
+        }
+    }
+}
+
+private struct SelectedLandmarkKey: FocusedValueKey {
+    typealias Value = Binding<Landmark>
+}
+
+
+extension FocusedValues {
+    var selectedLandmark: Binding<Landmark>? {
+        get { self[SelectedLandmarkKey.self] }
+        set { self[SelectedLandmarkKey.self] = newValue }
+    }
+}
