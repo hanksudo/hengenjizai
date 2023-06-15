9b22ceda09f8607aebbdd9a31fb69da73aa5dc98
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-06-15 10:59:15 +0900

    Showcasae: library view

--- swift/Showcase/Showcase/Views/LibraryView.swift
+++ swift/Showcase/Showcase/Views/LibraryView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct LibraryView: View {
+    enum Flavor: String, CaseIterable, Identifiable {
+        case chocolate, vanilla, strawberry
+        var id: Self { self }
+    }
+
+    @State private var selectedFlavor: Flavor = .chocolate
+    
     var body: some View {
         VStack {
             HStack {
@@ -19,9 +26,44 @@ struct LibraryView: View {
             .font(.largeTitle)
             
             Divider()
+            
             Label("Favorite Books", systemImage: "books.vertical")
                 .labelStyle(.titleAndIcon)
                 .font(.largeTitle)
+            
+            Divider()
+            
+            HStack {
+                Picker("Flavor", selection: $selectedFlavor) {
+                    Text("Chocolate").tag(Flavor.chocolate)
+                    Text("Vanilla").tag(Flavor.vanilla)
+                    Text("Strawberry").tag(Flavor.strawberry)
+                }
+                Button("OK") {}
+            }
+            .controlSize(.large)
+            
+            Divider()
+            
+            AsyncImage(url: URL(string: "https://picsum.photos/200/200")) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 200, height: 200)
+            
+            Divider()
+            
+            HStack {
+                Rectangle()
+                    .foregroundColor(.blue)
+                Circle()
+                    .foregroundColor(.orange)
+                RoundedRectangle(cornerRadius: 15, style: .continuous)
+                    .foregroundColor(.green)
+            }
+            .aspectRatio(3.0, contentMode: .fit)
+            .padding()
         }
     }
 }
