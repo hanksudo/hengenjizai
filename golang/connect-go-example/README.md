acc4fe55d2a1e208e69e4c40a2ac260904891ac6
Hank Wang <467745+hanksudo@users.noreply.github.com>
2023-08-04 12:52:15 +0900

    update connect-go-example

--- golang/connect-go-example/README.md
+++ golang/connect-go-example/README.md
@@ -15,3 +15,28 @@ go install connectrpc.com/connect/cmd/protoc-gen-connect-go@latest
 buf lint
 buf generate
 ```
+
+## Test
+
+```bash
+go run ./cmd/server/main.go
+```
+
+Made request by curl and grpcurl
+
+```bash
+curl \
+    --header "Content-Type: application/json" \
+    --data '{"name": "Hank"}' \
+    http://localhost:8080/greet.v1.GreetService/Greet
+grpcurl \
+    -protoset <(buf build -o -) -plaintext \
+    -d '{"name": "Hank"}' \
+    localhost:8080 greet.v1.GreetService/Greet
+```
+
+Made request by go client
+
+```bash
+go run ./cmd/client/main.go
+```
---
+++ golang/connect-go-example/cmd/server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	greetv1 "example/gen/greet/v1"
+	"example/gen/greet/v1/greetv1connect"
+	"fmt"
+	"log"
+	"net/http"
+
+	"connectrpc.com/connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+type GreetServer struct{}
+
+func (s *GreetServer) Greet(
+	ctx context.Context,
+	req *connect.Request[greetv1.GreetRequest],
+) (*connect.Response[greetv1.GreetResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&greetv1.GreetResponse{
+		Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
+	})
+	res.Header().Set("Greet-Version", "v1")
+	return res, nil
+}
+
+func main() {
+	greeter := &GreetServer{}
+	mux := http.NewServeMux()
+	path, handler := greetv1connect.NewGreetServiceHandler(greeter)
+	mux.Handle(path, handler)
+
+	http.ListenAndServe(
+		":8080",
+		// Use h2c so we can serve HTTP/2 without TLS.
+		// https://pkg.go.dev/golang.org/x/net/http2/h2c
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+}
